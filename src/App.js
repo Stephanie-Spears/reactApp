fd047060816124047ab8b685e4f69d067f2d0727
Stephanie Spears <Stephanie.Spears1@gmail.com>
2018-03-31 10:46:06 -0700

    update logo

--- src/App.js
+++ src/App.js
@@ -8,10 +8,10 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Gachapin! What you doing!</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Gachapin, you so silly. Where you going?
         </p>
       </div>
     );
